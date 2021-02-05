1bdbe56379dedf9efe3d6cd6db790ef15139f7f8
Dreamwalker <aristojeff@gmail.com>
2021-02-05 23:18:43 +0900

    [Add] pimp_my_button

--- lib/src/ui/mobile/quest/battle_page.dart
+++ lib/src/ui/mobile/quest/battle_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pimp_my_button/pimp_my_button.dart';
 
 class BattlePage extends StatefulWidget {
   @override
@@ -23,22 +24,34 @@ class _BattlePageState extends State<BattlePage> {
           SizedBox(
             height: MediaQuery.of(context).size.height / 2,
             width: MediaQuery.of(context).size.width / 1.2,
-            child: Card(
-              child: InkWell(
-                onTap: (){
-                  setState(() {
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  top: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      "assets/img/animation_640_kksar3b6.gif",
+                      fit: BoxFit.cover,
+                    ),
+                  )),
 
-                    damage++;
-                  });
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Image.asset(
-                    "assets/img/animation_640_kksar3b6.gif",
-                    fit: BoxFit.cover,
+                  PimpedButton(
+                    particle: DemoParticle(),
+                    pimpedWidgetBuilder: (context, controller) {
+                      return InkWell(
+                        onTap: (){
+                          controller.forward(from: 0.0);
+                        },
+
+                      );
+                    },
                   ),
-                ),
-              ),
+
+              ],
             ),
           ),
           
