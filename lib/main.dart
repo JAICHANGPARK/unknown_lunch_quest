964ef38ba1d18a4e704c048eb0daf5a82aa63568
Dreamwalker <aristojeff@gmail.com>
2021-02-13 15:19:50 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lunch_quest/src/routes/arg_quest_date.dart';
 import 'package:flutter_lunch_quest/src/ui/mobile/about_page.dart';
+import 'package:flutter_lunch_quest/src/ui/mobile/admin/admin_login_log_page.dart';
 import 'package:flutter_lunch_quest/src/ui/mobile/admin/admin_page.dart';
 import 'package:flutter_lunch_quest/src/ui/mobile/admin/pwd_change_page.dart';
-import 'package:flutter_lunch_quest/src/ui/mobile/admin/ticket_record_page.dart';
+import 'package:flutter_lunch_quest/src/ui/mobile/admin/ticket/ticket_record_use_page.dart';
 import 'package:flutter_lunch_quest/src/ui/mobile/record/mobile_record_page.dart';
 import 'package:flutter_lunch_quest/src/ui/wide_screen/wide_home_page.dart';
 
 import 'package:responsive_builder/responsive_builder.dart';
 import 'src/remote/api.dart';
 import 'src/ui/mobile/admin/login_page.dart';
+import 'src/ui/mobile/admin/ticket/ticket_record_page.dart';
 import 'src/ui/mobile/black_market/bit_coin_page.dart';
 import 'src/ui/mobile/bulletin_board_page.dart';
 import 'src/ui/mobile/contact_page.dart';
 import 'src/ui/mobile/home_page.dart';
 import 'src/ui/mobile/quest/battle_page.dart';
-import 'src/ui/mobile/record/mobile_record_print_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -75,7 +75,9 @@ class MyApp extends StatelessWidget {
         "/admin/login": (context) => LoginPage(),
         "/admin/home": (context) => AdminPage(),
         "/admin/home/pwd_change": (context) => PwdChangePage(),
-        "/admin/home/ticket/record": (context) => TicketRecordPage()
+        "/admin/home/ticket/record/buy": (context) => TicketRecordPage(),
+        "/admin/home/ticket/record/use": (context) => TicketUseRecordPage(),
+        "/admin/home/login/record": (context) => AdminLoginLogPage()
       },
     );
     // MyHomePage(title: 'Lunch Quest'),
