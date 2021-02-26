28f844741350ef3422046d98fb1a9f0e7b374e63
Dreamwalker <aristojeff@gmail.com>
2021-02-26 19:22:09 +0900

    Update enter_user_list_page.dart

--- lib/src/ui/mobile/enter_user_list_page.dart
+++ lib/src/ui/mobile/enter_user_list_page.dart
@@ -17,32 +17,40 @@ class _EnterUserListPageState extends State<EnterUserListPage> {
   List<mUser.User> bentoUserList = []; // 참가한 사용자 리스트를 담는 변수
 
 
-  Future fetchEnterUserList() async {
+  Future<bool> fetchEnterUserList() async {
     if (enterUserList.length > 0) enterUserList.clear();
     if (normalUserList.length > 0) normalUserList.clear();
     if (bentoUserList.length > 0) bentoUserList.clear();
     String selectDate = await readSelectDate();
+
     DocumentSnapshot querySnapshot =
         await FirebaseInstance.instance.fireStore.collection("lunch").doc(selectDate.split(" ").first).get();
 
     querySnapshot.data()["users"].forEach((element) {
+
       String part = "";
-      if (element.toString().split(",").length == 1) {
+      String sElement = element.toString();
+      // print(sElement.toString());
+      if (sElement.toString().split(",").length == 1) {
         part = "일반";
       } else {
-        part = element.toString().split(",")[1];
+        part = sElement.toString().split(",")[1];
       }
-      String name = element.toString().split(",").first;
-      String team = element.toString().split(",").length > 2 ? element.toString().split(",").last : "";
+      String name = sElement.toString().split(",").first;
+      // print("name: $name");
+      String team = sElement.toString().split(",").length > 2 ? sElement.toString().split(",").last : "";
       enterUserList.add(mUser.User(name: name, team: team, part: part));
     });
     normalUserList = enterUserList.where((element) => element.part =="일반").toList();
     bentoUserList = enterUserList.where((element) => element.part =="도시락").toList();
     setState(() {});
+    return true;
   }
 
-  Future refreshEnterUserList() async {
+  Future<List<mUser.User>> refreshEnterUserList() async {
     if (enterUserList.length > 0) enterUserList.clear();
+    if (normalUserList.length > 0) normalUserList.clear();
+    if (bentoUserList.length > 0) bentoUserList.clear();
     setState(() {});
     String selectDate = await readSelectDate();
     DocumentSnapshot querySnapshot =
@@ -61,13 +69,21 @@ class _EnterUserListPageState extends State<EnterUserListPage> {
     });
 
     setState(() {});
+    return enterUserList;
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    fetchEnterUserList();
+    Future.delayed(Duration(milliseconds: 250), (){
+      // print("delay done");
+      fetchEnterUserList().then((value) {
+        // print("fetchEnterUserList done");
+        setState(() {
+        });
+      });
+    });
   }
 
   @override
@@ -75,53 +91,56 @@ class _EnterUserListPageState extends State<EnterUserListPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("신청목록"),
+        actions: [
+          IconButton(icon: Icon(Icons.refresh), onPressed: (){
+            fetchEnterUserList();
+          })
+        ],
       ),
-      body: RefreshIndicator(
-          onRefresh: refreshEnterUserList,
-          child: enterUserList.length > 0
-              ? Row(
-                children: [
-                  Expanded(
-                    child: ListView.separated(
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            title: Text(normalUserList[index].name),
-                            trailing: Text(normalUserList[index].part),
-                          );
-                        },
-                        separatorBuilder: (context, index) {
-                          return Divider(
-                            height: 8,
-                            color: Colors.grey,
-                            endIndent: 8,
-                            indent: 8,
-                          );
-                        },
-                        itemCount: normalUserList.length),
-                  ),
-                  Expanded(
-                    child: ListView.separated(
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            title: Text(bentoUserList[index].name),
-                            trailing: Text(bentoUserList[index].part),
-                          );
-                        },
-                        separatorBuilder: (context, index) {
-                          return Divider(
-                            height: 8,
-                            color: Colors.grey,
-                            endIndent: 8,
-                            indent: 8,
-                          );
-                        },
-                        itemCount: bentoUserList.length),
-                  ),
-                ],
-              )
-              : Center(
-                  child: CircularProgressIndicator(),
-                )),
+      body: enterUserList.length > 0
+          ? Row(
+            children: [
+              Expanded(
+                child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(normalUserList[index].name),
+                        trailing: Text(normalUserList[index].part),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return Divider(
+                        height: 8,
+                        color: Colors.grey,
+                        endIndent: 8,
+                        indent: 8,
+                      );
+                    },
+                    itemCount: normalUserList.length),
+              ),
+              Expanded(
+                child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(bentoUserList[index].name),
+                        trailing: Text(bentoUserList[index].part),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return Divider(
+                        height: 8,
+                        color: Colors.grey,
+                        endIndent: 8,
+                        indent: 8,
+                      );
+                    },
+                    itemCount: bentoUserList.length),
+              ),
+            ],
+          )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
